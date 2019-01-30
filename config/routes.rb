5991c597cb9c87573cd15870c5ad8750c0a90bfd
amundskm <amundskm@gmail.com>
2019-01-30 13:26:40 -0600

    add error messages to sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  resources :user
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get '/signup',   to: 'users#new'
-  resources :users
+  get '/signup',   to: 'user#new'
+
 end
