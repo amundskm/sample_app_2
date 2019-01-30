992fc3d816c522f6ee14a4bc697ec11f51624592
amundskm <amundskm@gmail.com>
2019-01-30 14:20:20 -0600

    valid signups

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.field_with_errors"
 
   end
+
+  test "valid signup informatin" do
+    get signup_path
+    assert_difference 'User.count' do
+      assert_select 'form[action="/signup"]'
+      post signup_path, params: { user: {name:                  "Example User",
+                                        email:                 "user@example.com",
+                                        password:              "password",
+                                        password_confirmation: "password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
