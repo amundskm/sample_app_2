3d05f18f2c24d05d157bbf60a83c38deca03b14d
amundskm <amundskm@gmail.com>
2019-01-31 14:16:23 -0600

    advanced login added

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -42,12 +42,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not_empty cookies['remember_token']
   end
 
-  test "log in without remembering"
+  test "log in without remembering" do
     # Log in to set the cookie.
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
   end
-
 end
