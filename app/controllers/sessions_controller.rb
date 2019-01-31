b8afffb5b5a8ecf9cde218a3363f154635661dab
amundskm <amundskm@gmail.com>
2019-01-31 11:21:57 -0600

    update header links for logged in and logged out users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      # Valid login
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = "invalid email/password combination"
+      flash.now[:danger] = 'invalid email/password combination'
       render 'new'
     end
   end
 
-  def new
-  end
+  def new; end
 
-  def destroy
-  end
-
+  def destroy; end
 end
