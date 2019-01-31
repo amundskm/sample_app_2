90e034f404c15b73b6ec8126048802eb322567ca
amundskm <amundskm@gmail.com>
2019-01-31 10:42:37 -0600

    invalid log in attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(emai: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = "invalid email/password combination"
+      render 'new'
+    end
+  end
+
   def new
   end
+
+  def destroy
+  end
+
 end
