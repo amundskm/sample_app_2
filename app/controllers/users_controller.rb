7c1b214f29ccf4bd0324f945f12eef823348d819
amundskm <amundskm@gmail.com>
2019-02-07 09:50:30 -0600

    rearrange user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,23 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
+  def index
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,28 +31,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:sucess] = "User deleted"
-    redirect_to users_url
-  end
-
-  def edit
-  end
-
-  def index
-    @users = User.where(activated: true).paginate(page: params[:page])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
-  end
-
   def update
     if @user.update_attributes(user_params)
       flash[:sucess] = "Profile updated"
@@ -43,6 +38,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:sucess] = "User deleted"
+    redirect_to users_url
   end
 
   private
